3230e83ea9525122d36566f4d80388a1a3e08645
jolo <jgutierrez1100@gmail.com>
2021-03-23 09:39:15 +0800

    platforms now detect when player is on them

---
+++ Assets/Scripts/PlatformController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformController : MonoBehaviour
+{
+    private Rigidbody _rb;
+    private Renderer _renderer;
+    private Color _color;
+
+    public bool isActive;
+
+    [SerializeField] private float moveSpeed;
+    
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody>();
+        _renderer = GetComponent<Renderer>();
+    }
+
+    private void Start()
+    {
+        _color = _renderer.material.color;
+    }
+
+    private void MovePlatform()
+    {
+        
+    }
+    
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.GetComponent<PlayerMovement>())
+        {
+            print(other.gameObject.name);
+            _renderer.material.SetColor("_Color", Color.green);
+        }
+    }
+
+    private void OnCollisionExit(Collision other)
+    {
+        if (other.gameObject.GetComponent<PlayerMovement>())
+        {
+            _renderer.material.SetColor("_Color", _color);
+        }
+    }
+}
