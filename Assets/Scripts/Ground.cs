91866703977e21769461872553b0c8699b7bd087
jolo <jgutierrez1100@gmail.com>
2021-04-01 11:49:02 +0800

    platforms now raise randomly

--- Assets/Scripts/Ground.cs
+++ Assets/Scripts/Ground.cs
@@ -77,19 +77,25 @@ public class Ground : MonoBehaviour
     private Material _material;
     private Color _colorBase;
 
-    public bool isActive, hasPlayer;
+    public bool isActive, hasPlayer, isRaised;
     
     private void Awake()
     {
         _material = GetComponent<Renderer>().material;
         _colorBase = _material.color;
-        PlatformController.Controller.grounds.Add(this);
     }
 
+    private void Start()
+    {
+        PlatformController.Controller.grounds.Add(this);
+    }
+    
     private void Update()
     {
         if (isActive)
             _material.color = hasPlayer ? Color.red : Color.yellow;
+        else if (isRaised)
+            _material.color = hasPlayer ? Color.red : Color.cyan;
         else
             _material.color = hasPlayer ? Color.green : _colorBase;
     }
--- Assets/Scripts/PlatformController.cs
+++ Assets/Scripts/PlatformController.cs
@@ -10,6 +10,7 @@ public class PlatformController : MonoBehaviour
     public List<Ground> grounds;
 
     [SerializeField] private float raiseSpeed, raiseDuration;
+    [SerializeField] private float repeatMin, repeatMax;
     
     private void Awake()
     {
@@ -18,16 +19,47 @@ public class PlatformController : MonoBehaviour
 
     private void Start()
     {
+        InvokeRepeating("RaisePlatform",1f,Random.Range(repeatMin,repeatMax));
+    }
+
+    private void RaisePlatform()
+    {
         StartCoroutine(Raise());
     }
 
     private IEnumerator Raise()
     {
+        var timer = raiseDuration;
         var ground = grounds[Random.Range(0, grounds.Count)];
         var rb = ground.GetComponent<Rigidbody>();
-        ground.isActive = true;
-        yield return new WaitForSeconds(raiseDuration);
-        ground.isActive = false;
+
+        if(!ground.isRaised){
+            while (timer > 0)
+            {
+                ground.isActive = true;
+                rb.MovePosition(rb.position + Vector3.up * (raiseSpeed * Time.fixedDeltaTime));
+                yield return new WaitForEndOfFrame();
+                timer -= Time.deltaTime;
+            }
+
+            rb.velocity = Vector3.zero;
+            ground.isActive = false;
+            ground.isRaised = true;
+        }
+        else
+        {
+            while (timer > 0)
+            {
+                ground.isActive = true;
+                rb.MovePosition(rb.position + Vector3.down * (raiseSpeed * Time.fixedDeltaTime));
+                yield return new WaitForEndOfFrame();
+                timer -= Time.deltaTime;
+            }
+
+            rb.velocity = Vector3.zero;
+            ground.isActive = false;
+            ground.isRaised = false;
+        }
     }
     
 }
