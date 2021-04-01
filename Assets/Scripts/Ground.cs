c7b45331d77c00209da40d00281237be790514dc
bulletproofpancake <57520402+bulletproofpancake@users.noreply.github.com>
2021-04-01 13:48:01 +0800

    Platforms (#2)
    * platforms now detect when player is on them
    
    * platforms without the player rise
    
    * there is now a singular platform controller
    
    * Raise is now controlled by platform controller
    
    * platforms now raise randomly
    
    * duplicated scene
    
    * implemented platforms to player
    
    * clamped platform height
    
    * added more platforms
    
    * removed redundant scenes

---
+++ Assets/Scripts/Ground.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using UnityEngine;
+
+public class Ground : MonoBehaviour
+{
+    #region old code
+    // private Rigidbody _rb;
+    // private Renderer _renderer;
+    // private Color _color;
+    //
+    // public bool isActive;
+    // public bool hasPlayer;
+    //
+    // [SerializeField] private float moveSpeed;
+    // [SerializeField] private float riseTime;
+    //
+    // private void Awake()
+    // {
+    //     _rb = GetComponent<Rigidbody>();
+    //     _renderer = GetComponent<Renderer>();
+    //     PlatformController.Controller.ground.Add(this);
+    // }
+    //
+    // private void Start()
+    // {
+    //     _color = _renderer.material.color;
+    // }
+    //
+    // public void RaisePlatform()
+    // {
+    //     if (hasPlayer) return;
+    //     StartCoroutine(Raise());
+    // }
+    //
+    // private IEnumerator Raise()
+    // {
+    //     var timer = riseTime;
+    //
+    //     while (timer > 0)
+    //     {
+    //         _renderer.material.color = Color.yellow;
+    //         _rb.MovePosition(_rb.position + Vector3.up * (moveSpeed * Time.fixedDeltaTime));
+    //         yield return new WaitForEndOfFrame();
+    //         timer -= Time.deltaTime;
+    //     }
+    //
+    //     _renderer.material.color = _color;
+    //     _rb.velocity = Vector3.zero;
+    //     //isActive = false;
+    // }
+    //
+    // private void OnCollisionEnter(Collision other)
+    // {
+    //     if (other.gameObject.GetComponent<PlayerMovement>())
+    //     {
+    //         hasPlayer = true;
+    //         print(other.gameObject.name);
+    //         _renderer.material.SetColor("_Color", Color.green);
+    //     }
+    // }
+    //
+    // private void OnCollisionExit(Collision other)
+    // {
+    //     if (other.gameObject.GetComponent<PlayerMovement>())
+    //     {
+    //         _renderer.material.color = _color;
+    //         hasPlayer = false;
+    //     }
+    // }
+    //
+    // public void ChangeColor(Color c)
+    // {
+    //     _renderer.material.color = c;
+    // }
+    #endregion
+
+    private Material _material;
+    private Color _colorBase;
+
+    public bool isActive, hasPlayer, isRaised;
+    
+    private void Awake()
+    {
+        _material = GetComponent<Renderer>().material;
+        _colorBase = _material.color;
+    }
+
+    private void Start()
+    {
+        PlatformController.Controller.grounds.Add(this);
+    }
+    
+    private void Update()
+    {
+        if (isActive)
+            _material.color = hasPlayer ? Color.red : Color.yellow;
+        else if (isRaised)
+            _material.color = hasPlayer ? Color.red : Color.cyan;
+        else
+            _material.color = hasPlayer ? Color.green : _colorBase;
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+            hasPlayer = true;
+    }
+
+    private void OnCollisionExit(Collision other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+            hasPlayer = false;
+    }
+}
---
+++ Assets/Scripts/PlatformController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class PlatformController : MonoBehaviour
+{
+    public static PlatformController Controller;
+    public List<Ground> grounds;
+
+    [SerializeField] private float raiseSpeed, raiseDuration;
+    [SerializeField] private float repeatMin, repeatMax;
+    
+    private void Awake()
+    {
+        Controller = this;
+    }
+
+    private void Start()
+    {
+        InvokeRepeating("RaisePlatform",1f,Random.Range(repeatMin,repeatMax));
+    }
+
+    private void RaisePlatform()
+    {
+        StartCoroutine(Raise());
+    }
+
+    private IEnumerator Raise()
+    {
+        var timer = raiseDuration;
+        var ground = grounds[Random.Range(0, grounds.Count)];
+        var rb = ground.GetComponent<Rigidbody>();
+
+        if(!ground.isRaised){
+            while (timer > 0)
+            {
+                ground.isActive = true;
+                rb.MovePosition(rb.position + Vector3.up * (raiseSpeed * Time.fixedDeltaTime));
+                yield return new WaitForEndOfFrame();
+                timer -= Time.deltaTime;
+            }
+
+            rb.velocity = Vector3.zero;
+            ground.isActive = false;
+            ground.isRaised = true;
+            rb.position = new Vector3(rb.position.x, Mathf.Clamp(rb.position.y, 0, 1), rb.position.z);
+        }
+        else
+        {
+            while (timer > 0)
+            {
+                ground.isActive = true;
+                rb.MovePosition(rb.position + Vector3.down * (raiseSpeed * Time.fixedDeltaTime));
+                yield return new WaitForEndOfFrame();
+                timer -= Time.deltaTime;
+            }
+
+            rb.velocity = Vector3.zero;
+            ground.isActive = false;
+            ground.isRaised = false;
+            rb.position = new Vector3(rb.position.x, Mathf.Clamp(rb.position.y, -1, 0), rb.position.z);
+        }
+    }
+    
+}
