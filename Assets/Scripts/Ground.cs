0609387eca94bd3601f1d8b14ed621088bc2eca1
jolo <jgutierrez1100@gmail.com>
2021-04-01 10:26:23 +0800

    there is now a singular platform controller

---
+++ Assets/Scripts/Ground.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ground : MonoBehaviour
+{
+    private Rigidbody _rb;
+    private Renderer _renderer;
+    private Color _color;
+
+    public bool isActive;
+    public bool hasPlayer;
+    
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float riseTime;
+
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody>();
+        _renderer = GetComponent<Renderer>();
+        PlatformController.Controller.ground.Add(this);
+    }
+
+    private void Start()
+    {
+        _color = _renderer.material.color;
+    }
+    
+    private void RaisePlatform()
+    {
+        if (hasPlayer) return;
+        StartCoroutine(Raise());
+    }
+    
+    private IEnumerator Raise()
+    {
+        var timer = riseTime;
+
+        while (timer > 0)
+        {
+            _renderer.material.color = Color.yellow;
+            _rb.MovePosition(_rb.position + Vector3.up * (moveSpeed * Time.fixedDeltaTime));
+            yield return new WaitForEndOfFrame();
+            timer -= Time.deltaTime;
+        }
+
+        _renderer.material.color = _color;
+        _rb.velocity = Vector3.zero;
+        //isActive = false;
+    }
+    
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.GetComponent<PlayerMovement>())
+        {
+            hasPlayer = true;
+            print(other.gameObject.name);
+            _renderer.material.SetColor("_Color", Color.green);
+        }
+    }
+
+    private void OnCollisionExit(Collision other)
+    {
+        if (other.gameObject.GetComponent<PlayerMovement>())
+        {
+            _renderer.material.color = _color;
+            hasPlayer = false;
+        }
+    }
+
+    public void ChangeColor(Color c)
+    {
+        _renderer.material.color = c;
+    }
+    
+}
--- Assets/Scripts/PlatformController.cs
+++ Assets/Scripts/PlatformController.cs
@@ -1,77 +1,19 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlatformController : MonoBehaviour
 {
-    private Rigidbody _rb;
-    private Renderer _renderer;
-    private Color _color;
+    public static PlatformController Controller;
+    public List<Ground> ground;
 
-    public bool isActive;
-    public bool hasPlayer;
-
-    [SerializeField] private float moveSpeed;
-    [SerializeField] private float riseTime;
-    
     private void Awake()
     {
-        _rb = GetComponent<Rigidbody>();
-        _renderer = GetComponent<Renderer>();
+        Controller = this;
     }
 
     private void Start()
     {
-        _color = _renderer.material.color;
-        Invoke("RaisePlatform", 2f);
-        
-    }
-
-    private void FixedUpdate()
-    {
-       
-    }
-
-    private void RaisePlatform()
-    {
-        if (hasPlayer) return;
-        StartCoroutine(Raise());
-    }
-    
-    private IEnumerator Raise()
-    {
-        var timer = riseTime;
-
-        while (timer > 0)
-        {
-            _renderer.material.color = Color.yellow;
-            _rb.MovePosition(_rb.position + Vector3.up * (moveSpeed * Time.fixedDeltaTime));
-            yield return new WaitForEndOfFrame();
-            timer -= Time.deltaTime;
-        }
-
-        _renderer.material.color = _color;
-        _rb.velocity = Vector3.zero;
-        //isActive = false;
-    }
-    
-    private void OnCollisionEnter(Collision other)
-    {
-        if (other.gameObject.GetComponent<PlayerMovement>())
-        {
-            hasPlayer = true;
-            print(other.gameObject.name);
-            _renderer.material.SetColor("_Color", Color.green);
-        }
-    }
-
-    private void OnCollisionExit(Collision other)
-    {
-        if (other.gameObject.GetComponent<PlayerMovement>())
-        {
-            _renderer.material.color = _color;
-            hasPlayer = false;
-        }
+        ground[Random.Range(0,ground.Count)].ChangeColor(Color.blue);
     }
 }
