cfe23d0e3b606b7c2e06890f3fb94c34d533463d
jolo <jgutierrez1100@gmail.com>
2021-04-02 17:13:52 +0800

    added timer that disconnects players  and returns them to lobby

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,11 +1,16 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
     public List<PlayerStatus> players;
-
+    public int levelDuration;
+    public Text timerUI;
+    
     private void Awake()
     {
         Instance = this;
@@ -14,9 +19,22 @@ public class GameManager : MonoBehaviour
 
     private void Start()
     {
+        StartCoroutine(CountDownLevel());
         SetRoles();
     }
 
+    private IEnumerator CountDownLevel()
+    {
+        float timer = levelDuration;
+        while (timer > 0)
+        {
+            timerUI.text = $"{timer:0}";
+            yield return new WaitForEndOfFrame();
+            timer -= Time.deltaTime;
+        }
+        GameOver();
+    }
+    
     private void SetRoles()
     {
         players[Random.Range(0, players.Count)].isHunter = true;
@@ -29,4 +47,15 @@ public class GameManager : MonoBehaviour
             }
         }
     }
+
+    private void GameOver()
+    {
+        //Disconnect players
+        //Return to Lobby
+        timerUI.text = string.Empty;
+        print("Game Over. Returning to lobby");
+        PhotonNetwork.Disconnect();
+        PhotonNetwork.LoadLevel(0);
+    }
+
 }
