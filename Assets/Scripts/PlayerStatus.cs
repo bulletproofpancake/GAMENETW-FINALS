a0419c1593462fca571b6eba7f42bcc2bccacc4e
jolo <jgutierrez1100@gmail.com>
2021-04-02 13:14:45 +0800

    hunter and runner status randomly assigned on start

---
+++ Assets/Scripts/PlayerStatus.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+
+public class PlayerStatus : MonoBehaviour
+{
+   public bool isHunter;
+   public SkinnedMeshRenderer skinRenderer;
+   private Material material;
+
+   private void Awake()
+   {
+      material = skinRenderer.material;
+   }
+
+   private void Start()
+   {
+      GameManager.Instance.players.Add(this);
+   }
+   
+   private void Update()
+   {
+      material.color = isHunter ? Color.magenta : Color.blue;
+   }
+}
