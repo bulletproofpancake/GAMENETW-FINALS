4e006738aa9f6679810d4a24beaa20c89043e9f2
jolo <jgutierrez1100@gmail.com>
2021-04-05 22:14:30 +0800

    added Maam's code

---
+++ Assets/Scripts/MissCode/ColorItem.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//Basically, eto yung ground niyo in my sample;
+public class ColorItem : MonoBehaviour
+{
+    private SpriteRenderer sr;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    //just a function that sets the color
+    public void ChangeColor(Color c)
+    {
+        sr.color = c;
+    }
+}
