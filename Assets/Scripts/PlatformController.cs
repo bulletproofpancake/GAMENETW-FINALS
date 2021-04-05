093065b3ab9fffddd5d215c3d4679df09c1dead8
bulletproofpancake <57520402+bulletproofpancake@users.noreply.github.com>
2021-04-05 23:57:54 +0800

    Environment network (#4)
    * platforms no longer raise, only activate
    
    * implemented rpc
    
    * environment seems to by synced
    
    * platforms raising are synced
    
    * added pun rpc to ground
    
    * colors now syncing

--- Assets/Scripts/PlatformController.cs
+++ Assets/Scripts/PlatformController.cs
@@ -26,51 +26,76 @@ public class PlatformController : MonoBehaviour
 
     private void Start()
     {
+        //Only the Master Client can Raise Platforms
+        if (!myPV.IsMine||!PhotonNetwork.IsMasterClient)
+            return;
         InvokeRepeating("RaisePlatform",1f,Random.Range(repeatMin,repeatMax));
+        
     }
 
+    #region PlatformRaise
     private void RaisePlatform()
     {
-        StartCoroutine(Raise());
-    }
-
-    private IEnumerator Raise()
-    {
-        var timer = raiseDuration;
         var ground = grounds[Random.Range(0, grounds.Count)];
-        //var rb = ground.GetComponent<Rigidbody>();
-
-        if(!ground.isRaised){
-            while (timer > 0)
-            {
-                ground.isActive = true;
-                //rb.MovePosition(rb.position + Vector3.up * (raiseSpeed * Time.fixedDeltaTime));
-                yield return new WaitForEndOfFrame();
-                timer -= Time.deltaTime;
-            }
-
-            //rb.velocity = Vector3.zero;
-            ground.isActive = false;
-            ground.isRaised = true;
-            //rb.position = new Vector3(rb.position.x, Mathf.Clamp(rb.position.y, 0, 1), rb.position.z);
-        }
-        else
-        {
-            while (timer > 0)
-            {
-                ground.isActive = true;
-                //rb.MovePosition(rb.position + Vector3.down * (raiseSpeed * Time.fixedDeltaTime));
-                yield return new WaitForEndOfFrame();
-                timer -= Time.deltaTime;
-            }
-
-            //rb.velocity = Vector3.zero;
-            ground.isActive = false;
-            ground.isRaised = false;
-            //rb.position = new Vector3(rb.position.x, Mathf.Clamp(rb.position.y, -1, 0), rb.position.z);
-        }
+        myPV.RPC("RPCRaisePlatform",RpcTarget.AllViaServer,ground.gameObject.GetComponent<PhotonView>().ViewID);
+    }
+    
+    [PunRPC]
+    private void RPCRaisePlatform(int viewID)
+    {
+        StartCoroutine(Raise(viewID));
     }
 
+    private IEnumerator Raise(int viewID)
+    {
+        yield return new WaitForSeconds(1.0f);
+        
+        foreach (var ground in grounds)
+        {
+            if (ground.gameObject.GetComponent<PhotonView>().ViewID == viewID)
+            {
+                var timer = raiseDuration;
+                var rb = ground.gameObject.GetComponent<Rigidbody>();
+
+                if(!ground.isRaised){
+                    while (timer > 0)
+                    {
+                        ground.isActive = true;
+                        ground.ChangeColor(Color.yellow);
+                        rb.MovePosition(rb.position + Vector3.up * (raiseSpeed * Time.fixedDeltaTime));
+                        yield return new WaitForEndOfFrame();
+                        timer -= Time.deltaTime;
+                    }
+
+                    rb.velocity = Vector3.zero;
+                    ground.isActive = false;
+                    ground.isRaised = true;
+                    ground.ChangeColor(Color.cyan);
+                    rb.position = new Vector3(rb.position.x, Mathf.Clamp(rb.position.y, 0, 1), rb.position.z);
+                }
+                else
+                {
+                    while (timer > 0)
+                    {
+                        ground.isActive = true;
+                        ground.ChangeColor(Color.yellow);
+                        rb.MovePosition(rb.position + Vector3.down * (raiseSpeed * Time.fixedDeltaTime));
+                        yield return new WaitForEndOfFrame();
+                        timer -= Time.deltaTime;
+                    }
+
+                    rb.velocity = Vector3.zero;
+                    ground.isActive = false;
+                    ground.isRaised = false;
+                    ground.ChangeColor(ground.colorBase);
+                    rb.position = new Vector3(rb.position.x, Mathf.Clamp(rb.position.y, -1, 0), rb.position.z);
+                }
+            }
+        }
+        
+    }
+    #endregion
+    
     public IEnumerator RemoveHunter(GameObject player)
     {
         yield return new WaitForEndOfFrame();
